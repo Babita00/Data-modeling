ecf71257bc300576a14a9b463334b078d0442f5d
Babita00 <babitaasth07@gmail.com>
2024-07-04 08:53:40 +0545

    order model updated

--- models/ecommerce/order.models.js
+++ models/ecommerce/order.models.js
@@ -1,13 +1,37 @@
 import mongoose from "mongoose";
+const orderItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
 const orderSchema = new mongoose.Schema(
   {
-    productId: {
+    customer: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Product",
+      ref: "User",
     },
-    quantity: {
+    orderItems: {
+      types: [orderItemSchema],
+      //we can do similar as we do in todos for subtodos
+    },
+    orderPrice: {
       type: Number,
       required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["PENDING", "CANCELLED", "DELIVERED"],
+      default: "PENDING",
     },
   },
   { timestamps: true }
